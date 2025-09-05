42c709a8c54596055ab53b6cffd487a8e0a9642a
Ironman-Hacker <f20250596@pilani.bits-pilani.ac.in>
2025-09-05 22:16:54 +0530

    question_1.py

--- IEEE_Recruitment_Task_1/question_1.py
+++ IEEE_Recruitment_Task_1/question_1.py
@@ -1,7 +1,7 @@
 n=int(input("Enter the value of n: "))
 for i in range(n):
-    for j in range(n - i):
+    for j in range(n - i): #Adding Blank spaces
         print(" ", end="")
-    for j in range(i+1):
+    for j in range(i+1): #Adding stars
         print("*", end="")
     print()
