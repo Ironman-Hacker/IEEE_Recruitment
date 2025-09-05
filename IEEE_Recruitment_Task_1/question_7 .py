628cbcf10133078442c393336ece5b97363b8d85
Ironman-Hacker <f20250596@pilani.bits-pilani.ac.in>
2025-09-05 22:20:36 +0530

    question_7 .py

--- IEEE_Recruitment_Task_1/question_7 .py
+++ IEEE_Recruitment_Task_1/question_7 .py
@@ -1,4 +1,4 @@
 list1 = list(map(int, input("Enter elements of first list separated by spaces: ").split()))
 list2 = list(map(int, input("Enter elements of second list separated by spaces: ").split()))
-intersection = list(set(list1) & set(list2))
+intersection = list(set(list1) & set(list2)) #it includes numbers which are present in both the lists.
 print(intersection)
